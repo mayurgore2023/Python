d71311ff9012f244ad8f739e123e54222cb227c0
MAYUR RAJENDRA GORE <125110603+mayurgore2023@users.noreply.github.com>
2023-02-19 12:44:29 +0530

    return add

---
+++ AQ4.py
@@ -0,0 +1,12 @@
+
+
+# Q. program which display 5 times "Jay ganesh...." on screen:
+
+def main():
+    i=0
+    while i < 5 :
+        print("Jay ganesh...")
+        i=i+1
+
+if __name__=="__main__":
+    main()
