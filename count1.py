47172223afc6f55325eb206ec5be7f3d48a9f89d
MAYUR RAJENDRA GORE <125110603+mayurgore2023@users.noreply.github.com>
2023-02-20 16:40:51 +0530

    digit count

--- count1.py
+++ count1.py
@@ -2,7 +2,7 @@
 #Q Program accepts number from user and return return count of digits in that number
 
 def main():
-    No=int(input())
+    No=int(input("Enter the number:))
     Digit_count=0
     while (No>0):
         Digit_count=Digit_count+1
