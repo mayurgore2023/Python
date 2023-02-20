0c82f16966a7b8f981e8b6b16edccbac67e05482
MAYUR RAJENDRA GORE <125110603+mayurgore2023@users.noreply.github.com>
2023-02-20 16:17:30 +0530

    number triangle

---
+++ pattern4.py
@@ -0,0 +1,15 @@
+
+#Q program accept one number from user and return number pattern
+
+def main():
+    print("Enter the input")
+    No=int(input())
+
+    for i in range (1,No+1):
+        for j in range(1, i+1 ):
+            print(j,end=" ")
+        print()
+
+
+if __name__=="__main__":
+    m
